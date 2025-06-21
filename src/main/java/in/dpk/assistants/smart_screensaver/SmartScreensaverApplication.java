a210e6f31d49b3efd7572b4d11dfa9983dc1ca34
Deepak <deepak.kr@live.com>
2025-06-21 15:05:08 +0530

    Initial Commit

---
+++ src/main/java/in/dpk/assistants/smart_screensaver/SmartScreensaverApplication.java
@@ -0,0 +1,13 @@
+package in.dpk.assistants.smart_screensaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartScreensaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartScreensaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dpk/assistants/smart_screensaver/SmartScreensaverApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dpk.assistants.smart_screensaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartScreensaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
